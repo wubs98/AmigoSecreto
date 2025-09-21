2f2a2562cdfbf61fd8db05e751505734099d1e15
Wubs98 <nates.bonates@gmail.com>
2025-09-21 13:43:48 -0300

    Funcionalidade: Adiconar Amigo Implementada

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let i = 0;
+let count = 0;
+
+function limparCampo(){
+
+    let campo = document.querySelector("input");
+    campo.value = "";
+
+}
+
+function percorreLista(){
+
+    let lista = document.getElementById("listaAmigos");
+    let amigo;
+
+    while (i < count){
+        amigo = document.createElement("li");
+        amigo.textContent = amigos[i];
+        lista.appendChild(amigo);
+        i++;
+    }
+
+}
+
+function adicionarAmigo() {
+
+    let amigo = document.querySelector("input").value;
+
+    if (amigo == "") {
+        alert("Por Favor, insira um nome");
+    } else {
+        amigos.push(amigo);
+        count++;
+        percorreLista();
+    }
+
+    limparCampo();
+}
+
+
